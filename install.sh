6bef40ccf37536bc315ab4acf789d65de3b8b419
MarwinBL <sergey.solovyov@outlook.com>
2023-01-07 13:55:40 +0300

    Add initial script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Phase 1
+docker-compose -f ./docker-compose-initiate.yaml up -d nginx
+docker-compose -f ./docker-compose-initiate.yaml up certbot
+docker-compose -f ./docker-compose-initiate.yaml down
+
+# some configurations for let's encrypt
+curl -L --create-dirs -o ./nginx/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+openssl dhparam -out ./nginx/letsencrypt/ssl-dhparams.pem 2048
